7e134796a8016f04540ce9a128653008b6d30cec
tolgatabanli <tolgatabanli@gmail.com>
2025-04-12 21:05:29 +0200

    added compare variable fxn with helpers, which compare identity of common variables in given two scripts

---
+++ R/compare_variables.R
@@ -0,0 +1,57 @@
+#' compare_variables
+#'
+#' @param script1 
+#' @param script2 
+#'
+#' @returns a list
+#' @export
+compare_variables <- function(script1, script2, variables_to_compare) {
+  env1 <- source_script_into_env(script1)
+  env2 <- source_script_into_env(script2)
+  
+  vars1 <- ls(env1)
+  vars2 <- ls(env2)
+  
+  if(missing(variables_to_compare)) {
+    variables_to_compare <- intersect(vars1, vars2)
+  }
+  
+  comparison_results <- sapply(variables_to_compare,
+                               function(var) {
+    var1_value <- get(var, envir = env1)
+    var2_value <- get(var, envir = env2)
+    if (inherits(var1_value, "data.frame") || inherits(var1_value, "tbl_df")){
+      var1_value <- normalize_df(var1_value)
+      var2_value <- normalize_df(var2_value)
+    }
+    identical(var1_value, var2_value)
+  })
+  
+  return(comparison_results)
+}
+
+
+# HELPERS ---
+
+source_script_into_env <- function(path_to_script) {
+  lines <- readLines(path_to_script)
+  
+  # Remove installs
+  disabled_calls <- c("install.packages", "BiocManager::install", "drive_auth", "drive_download")
+  patterns_to_disable <- paste0("^\\s*", gsub("\\.", "\\\\.", disabled_calls), "\\s*\\(")
+  
+  for (pattern in patterns_to_disable) {
+    lines <- gsub(pattern, "# \\0", lines, perl = TRUE)
+  }
+  
+  env <- new.env()
+  eval(parse(text = lines), envir = env)
+  return(env)
+}
+
+normalize_df <- function(df) {
+  df <- as.data.frame(df)           # Convert to data frame to remove tibble class
+  row.names(df) <- NULL             # Remove row names for comparison
+  return(df)
+}
+
