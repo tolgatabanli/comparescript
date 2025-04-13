d0ae41b4e922cd086d09ef607cb061efa2f2e3c4
tolgatabanli <tolgatabanli@gmail.com>
2025-04-13 19:24:38 +0200

    adjusted tests for change from logicals to numericals and added tests for names and weights given

--- tests/testthat/test-compare_variables.R
+++ tests/testthat/test-compare_variables.R
@@ -1,17 +1,42 @@
 test_that("A fully correct assignment assessed correctly", {
   expect_equal(unname(compare_variables("../example_scripts/reference.R",
                                  "../example_scripts/student_full_correct.R")),
-               rep(T, 9))
+               rep(1, 9))
 })
 
 test_that("An assignment with mistakes assessed correctly with respective vars", {
   expect_equal(unname(compare_variables("../example_scripts/reference.R",
                                         "../example_scripts/student_with_mistakes.R")),
-               c(F, T, T, T, T, T, F, T, T))
+               c(0, 1, 1, 1, 1, 1, 0, 1, 1))
 })
 
 test_that("A fullly wrong assignment (except data-reading and example manual_tbl) assessed correctly", {
   expect_equal(unname(compare_variables("../example_scripts/reference.R",
                                         "../example_scripts/student_full_wrong.R")),
-               c(F, T, rep(F, 5), T, F))
+               c(0, 1, rep(0, 5), 1, 0))
+})
+
+# Weights and names given
+test_that("A fully correct assignment assessed correctly with given var names and weights", {
+  expect_equal(unname(compare_variables("../example_scripts/reference.R",
+                                        "../example_scripts/student_full_correct.R",
+                                        c("summarized_income", "length_chick_dataset", "chick_summarized"),
+                                        c(2, 1, 2))),
+               c(2, 1, 2))
+})
+
+test_that("An assignment with mistakes assessed correctly with respective vars with given var names and weights", {
+  expect_equal(unname(compare_variables("../example_scripts/reference.R",
+                                        "../example_scripts/student_with_mistakes.R",
+                                        c("summarized_income", "length_chick_dataset", "chick_summarized"),
+                                        c(2, 1, 2))),
+               c(2, 1, 0))
+})
+
+test_that("A fullly wrong assignment (except data-reading and example manual_tbl) assessed correctly with given var names and weights", {
+  expect_equal(unname(compare_variables("../example_scripts/reference.R",
+                                        "../example_scripts/student_full_wrong.R",
+                                        c("summarized_income", "length_chick_dataset", "chick_summarized"),
+                                        c(2, 1, 2))),
+               c(0, 0, 0))
 })
