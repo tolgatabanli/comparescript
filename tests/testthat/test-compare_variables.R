6f1e04c0143768555193786130ef1825f377d1d7
tolgatabanli <tolgatabanli@gmail.com>
2025-04-13 19:41:18 +0200

    added test for not existing variable error

--- tests/testthat/test-compare_variables.R
+++ tests/testthat/test-compare_variables.R
@@ -10,7 +10,7 @@ test_that("An assignment with mistakes assessed correctly with respective vars",
                c(0, 1, 1, 1, 1, 1, 0, 1, 1))
 })
 
-test_that("A fullly wrong assignment (except data-reading and example manual_tbl) assessed correctly", {
+test_that("A fully wrong assignment (except data-reading and example manual_tbl) assessed correctly", {
   expect_equal(unname(compare_variables("../example_scripts/reference.R",
                                         "../example_scripts/student_full_wrong.R")),
                c(0, 1, rep(0, 5), 1, 0))
@@ -33,10 +33,19 @@ test_that("An assignment with mistakes assessed correctly with respective vars w
                c(2, 1, 0))
 })
 
-test_that("A fullly wrong assignment (except data-reading and example manual_tbl) assessed correctly with given var names and weights", {
+test_that("A fully wrong assignment (except data-reading and example manual_tbl) assessed correctly with given var names and weights", {
   expect_equal(unname(compare_variables("../example_scripts/reference.R",
                                         "../example_scripts/student_full_wrong.R",
                                         c("summarized_income", "length_chick_dataset", "chick_summarized"),
                                         c(2, 1, 2))),
                c(0, 0, 0))
 })
+
+# Error messages
+test_that("A variable that does not exist can be detected", {
+          expect_error(compare_variables("../example_scripts/reference.R",
+                                         "../example_scripts/student_full_wrong.R",
+                                         c("wrong_variable")),
+                       regexp = "Given variables should exist")
+  }
+)
